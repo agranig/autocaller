8b225b9e4cc2582074accbc5ac864ed53a896d38
Andreas Granig <andreas@granig.com>
2022-03-01 23:00:53 +0100

    Run dialer on docker instance start

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-#docker network create autocall
-#docker run --network=autocall --rm -ti -v $(pwd)/etc:/etc/baresip -p 8080:8000 baresip:latest
-
-docker run --rm -ti -v $(pwd)/etc:/etc/baresip -p 8080:8000 baresip:latest /bin/bash
+docker run --rm -ti -v $(pwd)/etc:/etc/baresip -p 8080:8000 baresip:latest
 
